5649405d1b326f0643ba5c469406b93320fcbcd4
IMRANDIL <aliimranadil2@gmail.com>
2021-07-01 19:32:36 +0530

    Updates

---
+++ precache-manifest.032e1d8c6a302b8593d26895558f5a0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd3d68c72867f5df9983f42f64b4ae13",
+    "url": "/Markdown_React/index.html"
+  },
+  {
+    "revision": "75ed35b5beb02493ab8f",
+    "url": "/Markdown_React/static/css/main.67d641af.chunk.css"
+  },
+  {
+    "revision": "6bbb2575db765f6d65f2",
+    "url": "/Markdown_React/static/js/2.9d0b99b7.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/Markdown_React/static/js/2.9d0b99b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75ed35b5beb02493ab8f",
+    "url": "/Markdown_React/static/js/main.65ec5a4b.chunk.js"
+  },
+  {
+    "revision": "00ed49a2fbf60fb3cc80",
+    "url": "/Markdown_React/static/js/runtime-main.451abfeb.js"
+  }
+]);
